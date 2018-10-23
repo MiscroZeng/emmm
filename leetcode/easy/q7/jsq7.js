903f71b7d6caa7ecf90199f4d796f15f0f5bbc6a
SanGo <512462572@qq.com>
2018-10-23 20:39:31 +0800

    git commit

--- leetcode/easy/q7/jsq7.js
+++ leetcode/easy/q7/jsq7.js
@@ -2,7 +2,7 @@ var reverse = function(x) {
     var target;
     if(x > 0 && x < Math.pow(2, 31) - 1){
         target = x.toString().split("").reverse().join("");
-        return target;
+        return Number(target);
     }else if( x > Math.pow(-2, 31) && x < 0){
         target = x.toString().split("").reverse();
         target.pop();
