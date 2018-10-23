a962b717bfd33d36457041bd947b3c2373302b0a
SanGo <512462572@qq.com>
2018-10-23 21:01:35 +0800

    found err... but now it is accepted program...

--- leetcode/easy/q7/jsq7.js
+++ leetcode/easy/q7/jsq7.js
@@ -1,14 +1,22 @@
 var reverse = function(x) {
     var target;
-    if(x > 0 && x < Math.pow(2, 31) - 1){
+    if(x > 0){
         target = x.toString().split("").reverse().join("");
-        return Number(target);
-    }else if( x > Math.pow(-2, 31) && x < 0){
+        // return Number(target);
+        target = Number(target)
+    }else if(x < 0){
         target = x.toString().split("").reverse();
         target.pop();
         target.unshift("-");
-        return Number(target.join(""));
+        // return Number(target.join(""));
+        target = Number(target.join(""));
     }else{
         return 0;
+    }
+
+    if(target >= (Math.pow(2, 31) - 1) || target <= Math.pow(-2, 31)){
+        return 0;
+    }else{
+        return target;
     }
 };
