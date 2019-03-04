8fe581475a474dcddd98cecfb1ab95c2789f2be9
SanGo <512462572@qq.com>
2019-03-04 11:52:19 +0800

    28题完结

--- leetcode/easy/q28/q28.js
+++ leetcode/easy/q28/q28.js
@@ -1,3 +1,7 @@
-var strStr = function(haystack, needle) {
-    
+var strStr = function (haystack, needle) {
+    const len = needle.length;
+    if (!needle.length) return 0
+    for (let i = 0, l = haystack.length; i <= l - len; i++) {
+        if (haystack.slice(i, i + len) === needle) return i;
+    }
 };
